a255c3f98f3d49aa39bf5daebba8fd9ca901f798
Daniel Perez <danielperez@unitec.edu>
2017-07-02 21:11:52 -0600

    calculating pmt

--- src/js/app.controller.js
+++ src/js/app.controller.js
@@ -1,6 +1,24 @@
 angular.module('myApp', []).controller('appCtrl', function($scope) {
 	var vm = this;
 
+	vm.inputs = {
+		presentValue: 0,
+		rate: 0,
+		numberOfPeriods: 0
+	}
+
+	vm.outputs = {
+		monthPay: 0,
+		totalPay: 0
+	}
+
+	vm.evaluate = function(){
+		vm.outputs.monthPay = PMT(vm.inputs.rate, 
+			vm.inputs.numberOfPeriods,vm.inputs.presentValue);
+		vm.outputs.totalPay = vm.outputs.monthPay * vm.inputs.numberOfPeriods;
+		vm.outputs.monthPay = Math.round(vm.outputs.monthPay * 100) / 100
+		vm.outputs.totalPay = Math.round(vm.outputs.totalPay * 100) / 100
+	}
 
 	function PMT(rate, numberOfPeriods, presentValue, fv, type) {
 		let pmt, pvif;
